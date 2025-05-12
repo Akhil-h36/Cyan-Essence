ad5d0b68133d56c4196c155e03e609faf609d337
Akhil-h36 <akhil36g@gmail.com>
2025-05-13 00:19:53 +0530

    added host

--- cyan/settings.py
+++ cyan/settings.py
@@ -28,8 +28,10 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!)
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['cyanessence.shop','www.cyanessence.shop']
-
+ALLOWED_HOSTS = ['cyanessence.shop','www.cyanessence.shop',
+'ec2-56-228-56-117.eu-north-1.compute.amazonaws.com',
+    'localhost',
+    '127.0.0.1']
 
 # Application definition
 
