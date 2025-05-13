a1ae6f418695192df75aad0012817345ac64b85d
Akhil-h36 <akhil36g@gmail.com>
2025-05-13 11:07:06 +0530

    Remove staticfiles 4

--- cyan/settings.py
+++ cyan/settings.py
@@ -217,16 +217,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 import os
+# Static files setup
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') 
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
-    os.path.join(BASE_DIR, 'user/static'),
-    os.path.join(BASE_DIR, 'authentication1/static'),
-    os.path.join(BASE_DIR, 'userprofile/static'),
-    os.path.join(BASE_DIR, 'adminapp/static'),
 ]
 
 
